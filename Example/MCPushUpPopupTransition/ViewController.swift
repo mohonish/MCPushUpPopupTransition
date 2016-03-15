cb318bf35d85cf9fbf2bcd7ab4672d57f85dbd3b
Mohonish Chakraborty <mohonish.c@gmail.com>
2016-03-15 19:04:25 +0530

    changed podspec and added transition classes

--- Example/MCPushUpPopupTransition/ViewController.swift
+++ Example/MCPushUpPopupTransition/ViewController.swift
@@ -8,17 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIViewControllerTransitioningDelegate {
+    
+    let customPresentAnimationController = CustomPresentAnimationController()
+    let customDismissAnimationController = CustomDismissAnimationController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "ShowAction" {
+            let toVC = segue.destinationViewController as UIViewController
+            toVC.transitioningDelegate = self
+            toVC.modalPresentationStyle = UIModalPresentationStyle.OverCurrentContext
+        }
+    }
+    
+    // MARK: - UIViewControllerTransitioning Delegate
+    
+    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return customPresentAnimationController
+    }
+    
+    func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return customDismissAnimationController
+    }
+    
 }
 
--- MCPushUpPopupTransition.podspec
+++ MCPushUpPopupTransition.podspec
@@ -9,7 +9,7 @@
 Pod::Spec.new do |s|
   s.name             = "MCPushUpPopupTransition"
   s.version          = "0.1.0"
-  s.summary          = "A simple bottom push up present and dimiss transition with alpha change."
+  s.summary          = "A simple bottom push up popup transition"
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
